8609625ba19f55dc199ca759c2f5401e76f2578b
lbauriau <laura.bauriaud@etu.toulouse-inp.fr>
2024-11-28 11:55:46 +0100

    interpolation

---
+++ utility.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Interpolation envoie une liste de taille N+1 avec N le degrés
+def interpolation(points):
+
+    ## Points est une liste de points = [[x1,y1],[x2,y2]...]
+
