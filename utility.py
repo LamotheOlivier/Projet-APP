ba1dac59e3a7b431ce877fafa7815d766c833e59
lbauriau <laura.bauriaud@etu.toulouse-inp.fr>
2024-12-12 08:35:02 +0100

    MAJ

--- utility.py
+++ utility.py
@@ -1,8 +1,40 @@
 import numpy as np
+import matplotlib
+
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import sys
 
-## Interpolation envoie une liste de taille N+1 avec N le degrés
-def interpolation(points):
+n = 1
+for arg in sys.argv:
+    if (n == 2):
+        nbPoint = int(arg)
+    n += 1
 
-    ## Points est une liste de points = [[x1,y1],[x2,y2]...]
+print(nbPoint)
+points = []
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('Affichage des points')
+
+
+def onpick(event):
+    if len(points) < nbPoint:
+        thisline = event.artist
+        xdata = thisline.get_xdata()
+        ydata = thisline.get_ydata()
+        ind = event.ind
+        point = tuple(zip(xdata[ind], ydata[ind]))
+        points.append(point)
+        print('onpick point:', point)
+    else:
+        print('already have {} points'.format(len(points)))
+
+
+#while (nbPoint > len(points)):
+fig.canvas.mpl_connect('pick_event', onpick)
+plt.show()
+
+
+np.interp()
